f04c5b4587509c8dae524a7714231a78a027851b
zerx <zerx@posteo.de>
2024-03-25 23:07:13 +0100

    test

--- home/default.nix
+++ home/default.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 
 {
-  imports = [ ./apps ];
+  imports = [ ./apps/.sh.nix ];
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "chris";

c2d005515868d483371ed7182529ded196de2004
zerx <zerx@posteo.de>
2024-03-25 19:54:28 +0100

    fix path

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -8,7 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ../profiles/gnome.nix
+      ../../profiles/gnome.nix
     ];
 
   # Bootloader.

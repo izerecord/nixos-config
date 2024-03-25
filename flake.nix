2d9eabdb9b72e7a3ad24edc2ceeb767ee16ef55a
zerx <zerx@posteo.de>
2024-03-25 19:49:53 +0100

    init

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "my first flake!";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }:
+    let
+      lib = nixpkgs.lib;
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      nixosConfigurations = {
+        nixdesktop = lib.nixosSystem {
+          inherit system;
+          modules = [ ./desktop/configuration.nix ];
+        };
+        nixlaptop = lib.nixosSystem {
+          inherit system;
+          modules = [ ./laptop/configuration.nix ];
+        };
+      };
+
+      homeConfigurations = {
+        chris = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [ ./home.nix ];
+        };
+      };
+    };
+}

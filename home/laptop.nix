5255e9e893f25597c846b3277b1a894a6177b23d
zerx <zerx@posteo.de>
2024-03-27 20:51:14 +0100

    add nvidia

--- home/laptop.nix
+++ home/laptop.nix
@@ -5,10 +5,12 @@
 
   # todo: hyprland monitors (?)
 
-  wayland.windowManager.hyprland.settings =
+  wayland.windowManager.hyprland =
     (if userSettings.wm == "hyprland" then
       {
-        "monitor" = "eDP-1,1920x1080@60,0x0,1";
+        settings = {
+          "monitor" = "eDP-1,1920x1080@60,0x0,1";
+        };
       }
     else { }
     );
--- system/hosts/laptop/configuration.nix
+++ system/hosts/laptop/configuration.nix
@@ -12,7 +12,7 @@
       ../../wm/gnome.nix
     ]
     ++ (if userSettings.wm == "hyprland" then [ ../../wm/hyprland.nix ] else [ ]);
-    # todo: exclude gnome if hyprland is activated!
+  # todo: exclude gnome if hyprland is activated!
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
@@ -80,6 +80,61 @@
     #media-session.enable = true;
   };
 
+  # --- HARDWARE & GRAPHICS ---
+
+  services.xserver.videoDrivers = [ "nvidia" ]; # or "nvidiaLegacy470 etc.
+  hardware =
+    {
+      # Enable OpenGL
+      #opengl = {
+      #  enable = true;
+      #  driSupport = true;
+      #  driSupport32Bit = true;
+      #};
+      # Load nvidia driver for Xorg and Wayland
+
+      nvidia = {
+
+        # Modesetting is required.
+        modesetting.enable = true;
+
+        # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+        # Enable this if you have graphical corruption issues or application crashes after waking
+        # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+        # of just the bare essentials.
+        powerManagement.enable = false;
+
+        # Fine-grained power management. Turns off GPU when not in use.
+        # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+        powerManagement.finegrained = false;
+
+        # Use the NVidia open source kernel module (not to be confused with the
+        # independent third-party "nouveau" open source driver).
+        # Support is limited to the Turing and later architectures. Full list of 
+        # supported GPUs is at: 
+        # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+        # Only available from driver 515.43.04+
+        # Currently alpha-quality/buggy, so false is currently the recommended setting.
+        open = false;
+
+        # Enable the Nvidia settings menu,
+        # accessible via `nvidia-settings`.
+        nvidiaSettings = true;
+
+        # Optionally, you may need to select the appropriate driver version for your specific GPU.
+        package = config.boot.kernelPackages.nvidiaPackages.stable;
+        prime = {
+          # Make sure to use the correct Bus ID values for your system!
+          intelBusId = "PCI:0:2:0";
+          nvidiaBusId = "PCI:0:1:0";
+        };
+      };
+    };
+
+
+
+
+
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
@@ -89,8 +144,8 @@
     description = "chris";
     extraGroups = [ "networkmanager" "wheel" ];
     #packages = with pkgs; [
-      #  firefox
-      #  thunderbird
+    #  firefox
+    #  thunderbird
     #];
   };
 
@@ -98,8 +153,9 @@
   services.xserver.displayManager.autoLogin.enable = true;
   services.xserver.displayManager.autoLogin.user = "chris";
 
-  # Allow unfree packages
+  # Allow unfree packages and accept licence
   nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.nvidia.acceptLicense = true;
 
   programs = {
     steam.enable = true;

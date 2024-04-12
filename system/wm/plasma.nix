4234b6b0e1bec0b9d78649a8674da7e3c616b113
zerx <zerx@posteo.de>
2024-04-12 17:15:27 +0200

    add  services.xserver.enable

--- system/wm/plasma.nix
+++ system/wm/plasma.nix
@@ -1,6 +1,9 @@
 { config, pkgs, ... }:
 
 {
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
   # Enable the KDE Plasma Desktop Environment.
   services.xserver.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;

4b15140cfacf278e736c6e2e76b1c82bad10b79f
zerx <zerx@posteo.de>
2024-05-03 23:56:23 +0200

    add autologin - final

--- system/wm/plasma.nix
+++ system/wm/plasma.nix
@@ -4,14 +4,18 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  # Enable the KDE Plasma Desktop Environment.
+  # autologin and sddm 
+  services.displayManager.autoLogin =
+    {
+      enable = true;
+      user = "chris";
+    };
   services.displayManager.sddm =
     {
       enable = true;
       wayland.enable = true;
-      autoLogin.enable = true;
       autoLogin.relogin = true;
-      #autologin.user = "chris";
     };
+  # Enable the KDE Plasma Desktop Environment.
   services.desktopManager.plasma6.enable = true;
 }

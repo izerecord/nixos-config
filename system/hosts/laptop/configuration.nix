8257b4973162b1c3d5e15b92fd5225ea64de4b75
zerx <zerx@posteo.de>
2024-06-02 18:14:38 +0200

    idk

--- system/hosts/laptop/configuration.nix
+++ system/hosts/laptop/configuration.nix
@@ -15,8 +15,8 @@
 
   networking.hostName = "nixlaptop"; # Define your hostname.
 
-  nix.settings.trusted-users = ["chris"];
-  #nix.settings.trusted-public-keys = [""];
+  nix.settings.trusted-users = [ "chris" ];
+  nix.settings.trusted-public-keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBctRmEoZPexi2HewhHwNGbga5BogklrwAzQM5+sZQGC chris@nixlaptop" ];
 
   # --- HARDWARE & GRAPHICS ---
   #boot.kernelPackages = pkgs.linuxKernel.packages.linux_6_1;

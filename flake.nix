37ac92a63aeb06fed40a356c7382688772ec47a6
zerx <zerx@posteo.de>
2024-06-02 13:11:06 +0200

    add cosmic fixes

--- flake.nix
+++ flake.nix
@@ -5,9 +5,13 @@
     nixpkgs.url = "nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nixos-cosmic = {
+      url = "github:lilyinstarlight/nixos-cosmic";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }:
+  outputs = { self, nixpkgs, home-manager, nixos-cosmic, ... }:
     let
       lib = nixpkgs.lib;
       system = "x86_64-linux";
@@ -15,17 +19,28 @@
       userSettings = {
         wm = "cosmic";
       };
+      cosmicmodule = [{
+        nix.settings = {
+          substituters = [ "https://cosmic.cachix.org/" ];
+          trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
+        };
+      }
+        nixos-cosmic.nixosModules.default];
     in
     {
       nixosConfigurations = {
         nixdesktop = lib.nixosSystem {
           inherit system;
-          modules = [ ./system/hosts/desktop/configuration.nix ];
+          modules = [ ./system/hosts/desktop/configuration.nix ]
+            ++ (if userSettings.wm == "cosmic" then cosmicmodule
+          else [ ]);
           specialArgs = { inherit userSettings; };
         };
         nixlaptop = lib.nixosSystem {
           inherit system;
-          modules = [ ./system/hosts/laptop/configuration.nix ];
+          modules = [ ./system/hosts/laptop/configuration.nix ]
+            ++ (if userSettings.wm == "cosmic" then cosmicmodule
+          else [ ]);
           specialArgs = { inherit userSettings; };
         };
       };
--- system/wm/cosmic.nix
+++ system/wm/cosmic.nix
@@ -1,29 +1,6 @@
-
 { config, pkgs, ... }:
 
 {
-  inputs = {
-    nixos-cosmic = {
-      url = "github:lilyinstarlight/nixos-cosmic";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
-
-  outputs = { self, nixpkgs, nixos-cosmic }: {
-    nixosConfigurations = {
-      # NOTE: change "host" to your system's hostname
-      host = nixpkgs.lib.nixosSystem {
-        modules = [
-          {
-            nix.settings = {
-              substituters = [ "https://cosmic.cachix.org/" ];
-              trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
-            };
-          }
-          nixos-cosmic.nixosModules.default
-          ./configuration.nix
-        ];
-      };
-    };
-  };
+  services.desktopManager.cosmic.enable = true;
+  services.displayManager.cosmic-greeter.enable = true;
 }
